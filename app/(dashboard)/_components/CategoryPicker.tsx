43dec50eb2ea1a3df0f96d3cf77cc5a180cbb397
rxhul18 <rahulshah0728@gmail.com>
2025-04-01 01:34:16 +0530

    Implement category selection with API integration and user authentication

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,13 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import { TransactionsType } from "@/lib/types";
-
+import { Category } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 interface Props {
   type: TransactionsType;
 }
-function CategoryPicker({type}: Props) {
-   return <div>CategoryPicker</div>
+
+function CategoryPicker({ type }: Props) {
+  const [open, setOpen] = useState(false)
+  const [value, setValue] = useState("")
+
+  const categoriesQuery = useQuery({
+    queryKey: ["categories", type],
+    queryFn: () => fetch(`/api/categories?type=${type}`).then((res) =>
+      res.json()
+    )
+  })
+
+  const selectedCategory = categoriesQuery.data?.find((category: Category) => category.name === value)
+  
+  return(
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {selectedCategory ? <CategoryRow category={selectedCategory} />: "Select category"}
+        </Button>
+      </PopoverTrigger>
+    </Popover>
+  )
 }
 
-export default CategoryPicker
+export default CategoryPicker;
+
+function CategoryRow({ category }: { category: Category }) {
+  return (
+    <div className="flex items-center gap-2">
+      <span role="img">{category.icon}</span>
+      <span>{category.name}</span>
+    </div>
+  )
+}
--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -67,6 +67,7 @@ function CreateTransactionDialog({trigger,type}:Props){
               <FormField 
                 control={form.control}
                 name="category"
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
                 render={(feild)=>(
                   <FormItem>
                     <FormLabel>Category</FormLabel>
